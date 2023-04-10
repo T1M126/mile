168861a8d7bce8e69d40e4f54fe3de14d2f3d94e
Egor <egorka.pomidorka333@gmail.com>
2023-04-10 20:47:29 +0400

    .gitignore

---
+++ src/main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) {
+        int amount = 15000;
+        int mile = 20;
+        int bonus = amount / mile;
+        System.out.println("Итоговый бонус: "+ bonus );
+        
+    }
+}
