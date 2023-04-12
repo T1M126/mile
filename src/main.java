f021b34fc4a10896ca1ee7e0b4c5486ec5db05f9
Egor <egorka.pomidorka333@gmail.com>
2023-04-12 19:42:40 +0400

    src/Main.java

--- src/main.java
+++
@@ -1,9 +0,0 @@
-public class main {
-    public static void main(String[] args) {
-        int amount = 15000;
-        int mile = 20;
-        int bonus = amount / mile;
-        System.out.println("Итоговый бонус: "+ bonus );
-        
-    }
-}
